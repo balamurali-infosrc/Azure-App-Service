ef6c24051ee62e09f53189a2c8c267f8dafbb26c
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-10-08 12:48:16 +0530

    AZureAppService

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,7 +75,7 @@ resource "azurerm_app_service_plan" "plan" {
 
 
 resource "azurerm_app_service" "app" {
-  name                = "${var.prefix}-webapp"
+  name                = "${var.prefix}-webappInfo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.plan.id
